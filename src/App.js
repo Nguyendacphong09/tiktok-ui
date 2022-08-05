0f58f9f24e356cba18813b0a68135196d13e38ac
Nguyendacphong09 <nguyendacphong09@gmail.com>
2022-08-05 19:19:13 +0700

    create Router , Page and set Layout for page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Fragment } from 'react';
+
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/layouts';
+
 function App() {
     return (
-        <div className="App">
-            <h1>Reactjs</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
