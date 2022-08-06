7fb15310acd7cf49a4b374dd6592168fa925d6b2
Nguyendacphong09 <nguyendacphong09@gmail.com>
2022-08-06 07:37:56 +0700

    update Default Layout and fix sass loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,17 @@
-
-const { override , useBabelRc } = require("customize-cra");
+const { override, useBabelRc, addWebpackModuleRule } = require('customize-cra');
 
 module.exports = override(
-   // eslint-disable-next-line react-hooks/rules-of-hooks
-   useBabelRc()
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useBabelRc(),
+    addWebpackModuleRule({
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+        ],
+    }),
 );
--- src/components/layouts/DefaultLayout/Sidebar/index.js
+++ src/components/layouts/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,9 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
 function Sidebar() {
-    return <h1> Sidebar</h1>;
+    return <div className={cx('wrapper')}></div>;
 }
 
 export default Sidebar;
